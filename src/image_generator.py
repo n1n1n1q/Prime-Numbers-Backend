f1effc77b82f79a3eac97cb5b49c59323fc23fd2
Oleh Basystyi <basystyo@gmail.com>
2024-04-05 21:43:22 +0300

    added more flexible image generating options

--- src/image_generator.py
+++ src/image_generator.py
@@ -2,28 +2,31 @@
 Texture generation
 """
 import textwrap
+import random
 from PIL import Image, ImageDraw, ImageFont
 import sieve
 TEXTURE_PATH = "assets/backrooms.png"
 FONT_PATH = "assets/caskadiya.ttf"
 FONT_SIZE = 60
-SIEVE_NUM = 10000
-SAVE_PATH = "result_image.jpg"
+SIEVE_NUM = 1000000
+SAVE_PATH = "res/result_image"
+ITERATIONS = 30
+for i in range(ITERATIONS):
+    texture_image= Image.open(TEXTURE_PATH)
 
-texture_image= Image.open(TEXTURE_PATH)
+    numbers_layout = Image.new('RGBA', texture_image.size, (255,255,255,0))
+    font = ImageFont.truetype(FONT_PATH,FONT_SIZE)
 
-numbers_layout = Image.new('RGBA', texture_image.size, (255,255,255,0))
-font = ImageFont.truetype(FONT_PATH,FONT_SIZE)
+    draw = ImageDraw.Draw(numbers_layout)
 
-draw = ImageDraw.Draw(numbers_layout)
+    primes = sieve.eratosthenes_sieve(SIEVE_NUM)
+    primes = [random.choice(primes) for _ in range(100)]
+    primes_txt = ' '.join([str(num) for num in primes])
+    wrapped = textwrap.fill(primes_txt,width=28)
 
-primes = sieve.eratosthenes_sieve(SIEVE_NUM)
-primes_txt = ' '.join([str(num) for num in primes])
-wrapped = textwrap.fill(primes_txt,width=28)
+    draw.text((0,0),wrapped,fill=(0,0,0,200),font=font)
 
-draw.text((0,0),wrapped,fill=(255,255,255,200),font=font)
-
-combined_img = Image.alpha_composite(texture_image.convert('RGBA'), numbers_layout)
-result_img = combined_img.convert('RGB')
-result_img.save(SAVE_PATH)
-result_img.show()
+    combined_img = Image.alpha_composite(texture_image.convert('RGBA'), numbers_layout)
+    result_img = combined_img.convert('RGB')
+    result_img.save(SAVE_PATH+str(i)+'.jpg')
+# result_img.show()
